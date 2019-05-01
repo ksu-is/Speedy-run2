f11144483c01e2283330156734602cd4f408d764
dmortiniera <48696865+dmortiniera@users.noreply.github.com>
2019-05-01 04:18:51 -0400

    Update Working code.py

--- Working code.py
+++ Working code.py
@@ -10,6 +10,8 @@ pygame.init()
 #screen resolution
 width = 1000
 height = 850
+#car boundary
+car_size= 55
 
 raceDisplay = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Speedy Run')
@@ -28,27 +30,33 @@ x =  (width * 0.45)
 y = (height * 0.8)
 
 while not speedover:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            speedover = True
-            #the code for control
-        if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            x=25
-        elif event.key == pygame.K_RIGHT:
-            x=25
-        elif event.key == pygame.K_UP:
-        	y=25
-        elif event.key == pygame.K_DOWN:
-        	y=25
+   if event.type == pygame.KEYDOWN:
+        	if event.key == pygame.K_LEFT:
+        	  x-=100
+        	elif event.key == pygame.K_RIGHT:
+        		x+=100
+        	elif event.key == pygame.K_UP:
+        		y-=50
+        	elif event.key == pygame.K_DOWN:
+        		y+=50
+        if x > width - car_size or x < 0:
+        	speedover= True
+
+        elif y>height - car_size or y< 0:
+        	speedover = True
+	 	e
 
         	car_position=[x,y]
-            
+#the control is working buy it goes like everywhere
+# I am going to set up boundaries  and fix it so it stay in palce after we put the background
 #backg fill
     raceDisplay.fill(white)
     car(x,y)
 
-        
+        if x > width - car_size or x < 0:
+            speedover =True
+        elif y>height - car_size or y< 0:
+             speedover = True
     pygame.display.update()
     #frame per second
     clock.tick(60)
