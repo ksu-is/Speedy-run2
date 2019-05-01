2b89898966b01e6fdbeb98506c6a26dceb785fab
dmortiniera <48696865+dmortiniera@users.noreply.github.com>
2019-05-01 01:44:03 -0400

    add the  picture and run it great

--- game.py
+++ game.py
@@ -3,23 +3,42 @@ import pygame
 
 pygame.init()
 # size and set the main display of the game.
+#adding variable for screen resolution'
+width = 1000
+height = 850 
 raceDisplay = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Speedy Run')
+#adding the color for our screen
+black = (0,0.0)
+white = (255.255.255)
+
 # This is for frame per second of the game. If we put high value in the () The game goes more smoothly.
 clock = pygame.time.Clock()
 
 speed = False
+#loading car image
+car1 = pygame.image.load('car1.png')
+#def the car1
+def car(x,y):
+    gameDisplay.blit(car1, (x,y))
+#make the starting point of our car
+x= 500
+y= 100
 
 while not speed:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             speed = True
+#make the backg color
+gameDisplay.fill(white)
+car(x,y)
+    
 
-        print(event)
 
     pygame.display.update()
     clock.tick()
-    
+  
+
 pygame.quit()
 quit()
