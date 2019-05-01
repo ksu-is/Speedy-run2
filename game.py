7a17301ad97f3553b4a9fd267d54ae482e68d283
yunukim <46737258+yunukim@users.noreply.github.com>
2019-05-01 14:23:43 +0900

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,24 @@
+import pygame
 
+pygame.init()
+
+raceDisplay = pygame.display.set_mode((1000,850))
+pygame.display.set_caption('Speedy Run')
+
+clock = pygame.time.Clock()
+
+speed = False
+
+while not speed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            speed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick()
+    
+pygame.quit()
+quit()
